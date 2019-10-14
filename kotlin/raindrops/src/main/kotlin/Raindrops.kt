ab3f2c23e24ee347637b98f5f29d1f8a15741108
Khalil <khalil@email.com>
2019-10-14 18:35:14 -0300

    Raindrops v2

--- kotlin/raindrops/src/main/kotlin/Raindrops.kt
+++ kotlin/raindrops/src/main/kotlin/Raindrops.kt
@@ -1,24 +1,11 @@
 class Raindrops {
     companion object {
-        fun convert(input: Int): String {
-            val factors = ArrayList<Int>()
-            var aux = input
-            while (aux > 1) {
-                if (input % aux == 0) {
-                    factors.add(0, aux)
-                }
-                aux--
-            }
-            var result = ""
-            factors.forEach { factor ->
-                when (factor) {
-                    3 -> result += "Pling"
-                    5 -> result += "Plang"
-                    7 -> result += "Plong"
-                }
-            }
-            if (result.isEmpty()) result = "$input"
-            return result
+        fun convert(input: Int): String = buildString {
+            if (input % 3 == 0) append("Pling")
+            if (input % 5 == 0) append("Plang")
+            if (input % 7 == 0) append("Plong")
+            if (isEmpty()) append(input)
+
         }
     }
 }
