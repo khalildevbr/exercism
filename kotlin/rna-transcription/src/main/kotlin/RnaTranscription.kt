f236e5461e093d08d1dacaa57d4c55012e021683
Khalil <khalil@email.com>
2019-10-14 16:25:49 -0300

    Rna-Transcription v1

---
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -0,0 +1,4 @@
+object RnaTranscription {
+    val dnaToRna = mapOf ('C' to 'G', 'G' to 'C', 'T' to 'A', 'A' to 'U')
+    fun transcribeToRna(dna: String): String = dna.map { dnaToRna[it] }.joinToString("")
+}
