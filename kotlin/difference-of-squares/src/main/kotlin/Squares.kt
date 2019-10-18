486c5a99df03d2c819ba7aad5c5421897c6160ac
Khalil <khalil@email.com>
2019-10-18 10:08:48 -0300

    Diff of Squares v2

--- kotlin/difference-of-squares/src/main/kotlin/Squares.kt
+++ kotlin/difference-of-squares/src/main/kotlin/Squares.kt
@@ -1,16 +1,16 @@
-import kotlin.math.pow
-
 class Squares(private val number: Int) {
 
     fun squareOfSum(): Int {
-        return 0.rangeTo(number).sum().toDouble().pow(2.0).toInt()
+        return 0.rangeTo(number).sum().pow()
     }
-    
+
     fun sumOfSquares(): Int {
-        return 0.rangeTo(number).sumBy { it.toDouble().pow(2.0).toInt() }
+        return 0.rangeTo(number).sumBy { it.pow() }
     }
 
     fun difference(): Int {
         return squareOfSum() - sumOfSquares()
     }
+
+    private fun Int.pow(): Int = this * this
 }
