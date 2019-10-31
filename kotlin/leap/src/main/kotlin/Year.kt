d1e708eb4bc67c30c33a40508ae896a213148320
Khalil <khalilayache@gmail.com>
2019-10-31 10:41:23 -0300

    Leap v2

--- kotlin/leap/src/main/kotlin/Year.kt
+++ kotlin/leap/src/main/kotlin/Year.kt
@@ -1,4 +1,3 @@
 class Year(private val year: Int) {
-    var isLeap: Boolean = leap()
-    private fun leap(): Boolean = year % 4 == 0 && ((year % 100 == 0 && year % 400 == 0) || year % 100 != 0)
+    val isLeap: Boolean = year % 4 == 0 && ((year % 100 == 0 && year % 400 == 0) || year % 100 != 0)
 }
