6bf9ae02449cc4f40de9c1ac30ecf4fb63446b21
Khalil <khalilayache@gmail.com>
2019-11-18 15:23:07 -0300

    Hamming v3

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,14 +1,16 @@
+import java.util.stream.IntStream;
+
 class Hamming {
     private int hamming;
 
     Hamming(String leftStrand, String rightStrand) {
         if (leftStrand.length() == rightStrand.length()) {
-            hamming = 0;
-            for (int i = 0; i < leftStrand.length(); i++) {
-                if (leftStrand.codePointAt(i) != rightStrand.codePointAt(i)) {
-                    hamming++;
-                }
-            }
+            hamming = IntStream
+                    .range(0, leftStrand.length())
+                    .reduce(0,
+                            (sum, position) -> leftStrand.charAt(position) != rightStrand.charAt(position) ?
+                                    ++sum : sum);
+
         } else {
             if (leftStrand.isEmpty())
                 throw new IllegalArgumentException("left strand must not be empty.");
