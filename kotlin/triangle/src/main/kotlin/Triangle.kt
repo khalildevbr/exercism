339d6edc6172693e9281ee2938d352bf5cff7a2d
Khalil <khalilayache@gmail.com>
2021-09-20 23:55:25 +0100

    Kotlin: Triangle v3

--- kotlin/triangle/src/main/kotlin/Triangle.kt
+++ kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -1,11 +1,13 @@
 class Triangle<out T : Number>(val firstSide: T, val secondSide: T, val thirdSide: T) {
+    private val sides: List<Double> = listOf(firstSide.toDouble(), secondSide.toDouble(), thirdSide.toDouble())
+    private val sidesSet = sides.toSet()
 
     init {
-        val sides = listOf(firstSide.toDouble(), secondSide.toDouble(), thirdSide.toDouble())
-        require(sides.all { side -> side > 0 } && sides.all { side -> sides.sum() - side >= side })
+        val sum = sides.sum()
+        require(sides.all { side -> side > 0 } && sides.all { side -> sum - side >= side })
     }
 
-    val isEquilateral: Boolean = setOf(firstSide, secondSide, thirdSide).size == 1
-    val isIsosceles: Boolean = setOf(firstSide, secondSide, thirdSide).size <= 2
-    val isScalene: Boolean = setOf(firstSide, secondSide, thirdSide).size == 3
+    val isEquilateral: Boolean = sidesSet.size == 1
+    val isIsosceles: Boolean = sidesSet.size <= 2
+    val isScalene: Boolean = sidesSet.size == 3
 }
